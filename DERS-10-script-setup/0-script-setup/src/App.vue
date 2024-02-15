02ffcc788d1602487e6bc7b5a16b806db7052eac
ssule <ssulecelepp@gmail.com>
2024-02-15 17:27:44 +0300

    Ders-9 tamamlandı.

---
+++ DERS-10-script-setup/0-script-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input type="text" v-model="title">
+  <button @click="inc">{{ counter }}</button>
+  <hr>
+  <oddOrEven :counter="counter" @odd-event="alertMe"/>
+  <hr>
+  <h1>User App</h1>
+  <input type="text" v-model="state.personal.name">
+  <input type="text" v-model="state.personal.lname">
+  {{ state.personal }}
+</template>
+
+<script setup>
+import {ref, reactive,watch} from "vue";
+import oddOrEven from "./components/oddOrEven.vue";
+import Utils from "./composables/Utils.js";
+
+const { title, counter, inc, alertMe }=Utils();
+
+// User App
+
+const state= reactive({
+  personal:{
+    name:null,
+    lname:null,
+
+  },
+});
+
+// () =>JSON.parse(JSON.stringify(state.personal))
+watch(() =>JSON.parse(JSON.stringify(state.personal)), (newPersonal,oldPersonal)=>{
+  console.log(oldPersonal);
+  console.log(newPersonal);
+  
+})
+
+
+</script>
--- DERS-9-composition-api/1-todo-list/src/App.vue
+++ DERS-9-composition-api/1-todo-list/src/App.vue
@@ -2,28 +2,42 @@
   <div class="w-screen h-screen bg-gray-400 pt-10">
     <div class="bg-gray-700 rounded-md shadow text-white w-1/3 mx-auto p-3">
       <h3 class="text-center text-2xl">Todo List</h3>
-      <section class="mt-4">
-        <input placeholder="Nedir?" type="text" class="w-full p-2 bg-gray-800 border-[1px] border-gray-400 rounded-md outline-none">
-      </section>
-
-      <section class="mt-4">
-        <div class="flex justify-between items-center even:bg-gray-600 px-2 py-1">
-          <label for="completed">
-            <input type="checkbox" id="completed" class="me-2"/>
-            <span class="text-lg">Todo 1</span> 
-          </label>
-          <button class="bg-indigo-500 px-2 py-1 rounded-md active:bg-indigo-800">Sil</button>
-        </div>
-        <div>
-          <small class="text-green-300">Tamamlanmışlar</small>
-        
-        </div>
-        <div>
-          <small class="text-red-400">Tamamlanmamışlar</small>
-        </div>
-        
-
-      </section>
+      <AddSection :AddTodo="AddTodo"/>
+      <TodoList @delete-event="deleteItem" :todoList="todoList"/>
     </div>
   </div>
 </template>
+
+<script>
+import AddSection from "./components/addSection.vue";
+import TodoList from "./components/todoList.vue";
+import {ref} from "vue";
+
+export default {
+  components: {
+    AddSection,
+    TodoList,
+  },
+  setup(){
+    const todoList = ref([]);
+    const AddTodo=(todoText)=>{
+      console.log(todoText);
+      todoList.value.push({
+        id: new Date().getTime(),
+        title: todoText,
+      })
+    };
+
+    const deleteItem =(todo)=>{
+      todoList.value = todoList.value.filter(t=> t.id != todo.id);
+
+    };
+
+    return {
+      todoList,
+      AddTodo,
+      deleteItem,
+    }
+  }
+};
+</script>
